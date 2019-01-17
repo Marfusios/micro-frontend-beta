25a8e73d2def567c972cf6b2da615c0ff5f0d75e
marekhoryna <marek.horyna@outlook.com>
2019-01-17 15:33:09 +0100

    added @angular/elements to bundle, fixed missing property

--- src/entry/buttons-example/buttons-example.component.ts
+++ src/entry/buttons-example/buttons-example.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsExampleComponent implements OnInit {
 
+  hideButton = false;
   validateLoading = false;
   submitLoading = false;
 
